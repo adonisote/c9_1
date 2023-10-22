aac01d58bc6d46119cb0f6ed5b3ff652856dbc1e
adonisote <adonis.almagro@code.berlin>
2023-10-22 21:24:24 +0200

    First Exanple of Chapter 9

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Dog():
+    def __init__(self, name, age):
+        self.random1 = name
+        self.random2 = age
+    def sit(self):
+        print(self.random1.title() + "is sitting now!")
+
+my_dog = Dog("pilatus", 3)
+my_dog.sit()
+
+x= 5
+if not x <= 4 or x > :
+   raise AssertionError('Oh no')
+
