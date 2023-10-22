69e1bb7363b5257a0b599eeb7d6b02dce6bebee0
adonisote <adonis.almagro@code.berlin>
2023-10-22 21:30:20 +0200

    9.1

--- classes.py
+++ classes.py
@@ -1,14 +1,24 @@
-class Dog():
-    def __init__(self, name, age):
-        self.random1 = name
-        self.random2 = age
-    def sit(self):
-        print(self.random1.title() + "is sitting now!")
+# class Dog():
+#     def __init__(self, name, age):
+#         self.random1 = name
+#         self.random2 = age
+#     def sit(self):
+#         print(self.random1.title() + "is sitting now!")
 
-my_dog = Dog("pilatus", 3)
-my_dog.sit()
+# my_dog = Dog("pilatus", 3)
+# my_dog.sit()
 
-x= 5
-if not x <= 4 or x > :
-   raise AssertionError('Oh no')
+# x= 5
+# if not x <= 4 or x > :
+#    raise AssertionError('Oh no')
+
+class Restaurant:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def describe_restaurant(self):
+        print(f'{self.name.title()}, {self.type()}')
+    def describe_restaurant(self):
+        print(f'{self.name.title()} is open now!')
 
